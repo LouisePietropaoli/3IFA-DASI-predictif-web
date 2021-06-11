13237522deea2083f8479570120a96c4a4899b0d
LouisePietropaoli <louise.pietropaoli@gmail.com>
2021-06-11 15:47:26 +0200

    gson 1

--- src/main/java/ActionServlet.java
+++ src/main/java/ActionServlet.java
@@ -1,3 +1,8 @@
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import dao.JpaUtil;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
@@ -5,6 +10,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import metier.data.Client;
+import metier.service.Service;
 
 @WebServlet(urlPatterns = {"/ActionServlet"})
 public class ActionServlet extends HttpServlet {
@@ -18,60 +25,56 @@ public class ActionServlet extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
+        response.setContentType("application/json;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet BONJOUR</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet BONJOUR at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+            Service service = new Service();
+
+            String todo = request.getParameter("todo");
+            JsonObject jsonContainer = new JsonObject();
+
+            switch (todo) {
+                case "se-connecter":
+                    Client client = service.authentifierClient(request.getParameter("login"), request.getParameter("password"));
+
+                    if (client != null) {
+                        jsonContainer.addProperty("connexion", true);
+                        JsonObject jsonClient = new JsonObject();
+                        Long id = client.getId();
+                        String nom = client.getNom();
+                        String prenom = client.getPrenom();
+                        String email = client.getEmail();
+                        jsonClient.addProperty("id", id);
+                        jsonClient.addProperty("nom", nom);
+                        jsonClient.addProperty("prenom", prenom);
+                        jsonClient.addProperty("email", email);
+                        jsonContainer.add("client", jsonClient);
+                    } else {
+                        jsonContainer.addProperty("connexion", false);
+
+                    }
+                    break;
+                default:
+                    break;
+            }
+            Gson gson = new GsonBuilder().setPrettyPrinting().serializeNulls().create();
+            gson.toJson(jsonContainer, out);
+            out.close();
+
         }
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
+    public void init() throws ServletException {
+        super.init();
+        JpaUtil.init();
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
+    public void destroy() {
+        JpaUtil.destroy();
+        super.destroy();
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
 }
