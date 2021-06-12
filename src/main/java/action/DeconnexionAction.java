0a59c0a57393619292e71d35c06699ceae1b30ea
LouisePietropaoli <louise.pietropaoli@gmail.com>
2021-06-12 20:55:32 +0200

    comments

--- src/main/java/action/DeconnexionAction.java
+++ src/main/java/action/DeconnexionAction.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package action;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-/**
- *
- * @author lpietropao
- */
 public class DeconnexionAction extends Action {
 
     @Override
