9652b58fef647be93449c33cfb3197cf2698d7ba
LouisePietropaoli <louise.pietropaoli@gmail.com>
2021-06-13 06:34:29 +0200

    switch avec commentaires mais pas les bonnes classes action serial

--- src/main/java/ActionServlet.java
+++ src/main/java/ActionServlet.java
@@ -51,19 +51,175 @@ public class ActionServlet extends HttpServlet {
             } else if (todo.equals("deconnecter")) {
                 // action = new DeconnexionAction();
                 //action.executer(request);
-               session.invalidate();
+                session.invalidate();
                 PrintWriter out = response.getWriter();
                 out.println("{}");
                 out.close();
             } else {
                 switch (todo) {
-                    case "details-client": {
+
+                    /**
+                     * utilisé pour la page d'accueil sans connexion et la liste
+                     * des mediums quand l'employé est connecté renvoie la liste
+                     * des mediums avec leur description simple (genre,
+                     * designation, type,description + id)
+                     */
+                    case "lister-mediums": {
                         action = new AuthentifierClientAction();
                         serialisation = new ClientSerialisation();
                     }
-
                     break;
 
+                    /**
+                     * utilisé pour l'initalisation de la page d'accueil client
+                     * authentifié renvoie la liste de médiums avec dispo et le
+                     * profil astral du client authentifié
+                     */
+                    case "initialser-accueil-client": {
+                        action = new AuthentifierClientAction();
+                        serialisation = new ClientSerialisation();
+                    }
+                    break;
+
+                    /**
+                     * utilisé pour la création d'un compte client pour ensuite
+                     * afficher la modale si la création a eu lieu ou signifier
+                     * une erreur sinon renvoie vrai si la création a eu lieu
+                     */
+                    case "creer-compte-client": {
+                        action = new AuthentifierClientAction();
+                        serialisation = new ClientSerialisation();
+                    }
+                    break;
+
+                    /**
+                     * utilisé pour afficher les détails d'un médiums et sa
+                     * disponbilité dans les modales renvoie le medium avec sa
+                     * description détaillée et le profil astral du client
+                     * authentifié
+                     */
+                    case "recuperer-details-medium": {
+                        action = new AuthentifierClientAction();
+                        serialisation = new ClientSerialisation();
+                    }
+                    break;
+
+                    /**
+                     * utilisé pour réserver un médium sur la page de listing
+                     * des mediums avec dispo et pour afficher la modale de
+                     * confirmation ou d'erreur de création de la consultation
+                     * renvoie vrai si la consutlation a été créée, faux sinon
+                     */
+                    case "reserver-medium": {
+                        action = new AuthentifierClientAction();
+                        serialisation = new ClientSerialisation();
+                    }
+                    break;
+
+                    /**
+                     * utilisé pour l'initialisation de la page de profil client
+                     * quand un client est authentifié renvoie le client
+                     * authentifié avec ses détails (sans ses consultations)
+                     */
+                    case "recuperer-details-profil-client": {
+                        action = new AuthentifierClientAction();
+                        serialisation = new ClientSerialisation();
+                    }
+                    break;
+
+                    /**
+                     * utilisé pour l'initalisation de la page d'historique de
+                     * consultations client renvoie les consultations du client
+                     * authentifié avec la disponibilité du médium pour chaque
+                     * consultation passée
+                     */
+                    case "recuperer-historique-consultations-client-avec-dispo": {
+                        action = new AuthentifierClientAction();
+                        serialisation = new ClientSerialisation();
+                    }
+                    break;
+                    /**
+                     * utilisé pour l'initalisation de la page de fiche client
+                     * (vue par un employé) renvoie toutes les infos du client
+                     * (dont profil astral) sauf son mot de passe L'historique
+                     * client n'est pas renvoyé
+                     */
+                    case "recuperer-fiche-client": {
+                        action = new AuthentifierClientAction();
+                        serialisation = new ClientSerialisation();
+                    }
+                    break;
+                    /**
+                     * utilisé pour afficher l'historique des consultations d'un
+                     * du client relié à l'employé authentifié renvoie
+                     * l'historique des consultations du client sans les dispo
+                     * des médiums associés
+                     */
+                    case "recuperer-historique-consultations-fiche-client": {
+                        action = new AuthentifierClientAction();
+                        serialisation = new ClientSerialisation();
+                    }
+                    break;
+
+                    /**
+                     * utilisé pour l'initalisation de la page d'accueil employé
+                     * authentifié renvoie la consultation reliée à ce medium
+                     * avec son statut si une consultation est associée au
+                     * médium, renvoie toutes les infos du client (dont profil
+                     * astral) sauf son mot de passe L'historique client n'est
+                     * pas renvoyé
+                     */
+                    case "initialser-accueil-employe": {
+                        action = new AuthentifierClientAction();
+                        serialisation = new ClientSerialisation();
+                    }
+                    break;
+
+                    /**
+                     * utilisé pour récupérer et afficher des prédictions
+                     * Renvoie les trois prédictions (amour, santé, travail)
+                     */
+                    case "demander-predictions": {
+                        action = new AuthentifierClientAction();
+                        serialisation = new ClientSerialisation();
+                    }
+                    break;
+
+                    /**
+                     * utilisé pour commencer la consultation de l'employé
+                     * authentifié et changer le bouton en "terminer la
+                     * conusltation" renvoie vraie si la consultation a bien été
+                     * démarrée, faux sinon
+                     */
+                    case "commencer-consultation": {
+                        action = new AuthentifierClientAction();
+                        serialisation = new ClientSerialisation();
+                    }
+                    break;
+
+                    /**
+                     * utilisé pour terminer une consultation avec saisie
+                     * facultative de commentaire et ne plus afficher la
+                     * consultation si elle s'est bien terminée renvoie vraie si
+                     * la consultation a bien été terminée, faux sinon
+                     */
+                    case "terminer-consultation": {
+                        action = new AuthentifierClientAction();
+                        serialisation = new ClientSerialisation();
+                    }
+                    break;
+
+                    /**
+                     * utilisé pour l'initalisation de la page des statistiques
+                     * d'un employé authentifié renvoie le nombre de
+                     * consultations, le nombre de clients et le pourcentage de
+                     * la clientèle totale de l'employé
+                     */
+                    case "recuperer-statistiques": {
+                        action = new AuthentifierClientAction();
+                        serialisation = new ClientSerialisation();
+                    }
+                    break;
                     default:
                         break;
                 }
