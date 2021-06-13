7efead3eeb484c8d40d7d6ffc3b7837870a6cd62
LouisePietropaoli <louise.pietropaoli@gmail.com>
2021-06-13 20:50:16 +0200

    liste mediums acceul sans connexion

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -33,7 +33,18 @@ function afficherListeMediums() {
     })
             .done(function (response) {
                 if (!response.erreur) {
+                    let = "";
                     console.log(response);
+                    const htmlMediumsListe = response.mediums.reduce(
+                            (accumulateur, valeurCourante) => {
+                        return accumulateur +=
+                                `<li class="carte-medium" data-id=
+                                "${valeurCourante.id}">
+                                ${valeurCourante.genre} | ${valeurCourante.designation} | 
+                                ${valeurCourante.type}<br/>
+                                ${valeurCourante.presentation}</li><br/>`;
+                    }, "");
+                    $("#liste-mediums").html(htmlMediumsListe);
                 } else {
                 }
             })
