a4e5fef1eef9daba36112d0116dade535f19c7f0
Alison <alison.lecuyer@etu.unice.fr>
2021-06-15 17:41:23 +0200

    Mise en place du CSS sur toutes les pages

--- src/main/webapp/js/fiche-client.js
+++ src/main/webapp/js/fiche-client.js
@@ -1,31 +1,20 @@
 $(document).ready(function () {
-    $.ajax({
-        url: './ActionServlet',
-        type: 'POST',
-        data: {
-            todo: 'recuperer-historique-consultations-client-avec-dispo'
-        },
-        dataType: 'json'
-    })
-            .done(function (response) {
-                if (!response.erreur) {
-                    console.log(response);
-               var cList = $('#historique');
-                    $.each(response.historiqueConsultation, function(i)
-                    {
-                        var li = $('<li/>')
-                            .appendTo(cList);
-                        var aaa = $('<a/>')
-                            .text(response.historiqueConsultation[i].dateDemande + " " + response.historiqueConsultation[i].heureDebut + " " + 
-                            response.historiqueConsultation[i].heureFin + " " + response.historiqueConsultation[i].nomMedium + " " + 
-                            response.historiqueConsultation[i].typeMedium + " " + response.historiqueConsultation[i].prenomEmploye + " " + 
-                            response.historiqueConsultation[i].nomEmploye).appendTo(li);
-                    });
-                } else {
+    $('#bouton-deconnexion').on('click', function () {
+        $.ajax({
+            url: './ActionServlet',
+            type: 'POST',
+            data: {
+                todo: 'deconnecter'
+            },
+            dataType: 'json'
+        })
+                .done(function () {
+                    window.location = `index.html`;
                 }
-            })
-            .fail(function (error) {
-            })
-            .always(function () {
-            });
+                )
+                .fail(function () { // Appel KO => erreur technique à gérer
+                })
+                .always(function () { // facultatif: appelé après le .done() ou le .fail()
+                });
+    });
 });
--- src/main/webapp/js/historique.js
+++ src/main/webapp/js/historique.js
@@ -26,4 +26,22 @@ $(document).ready(function () {
             })
             .always(function () {
             });
+    $('#bouton-deconnexion').on('click', function () {
+        $.ajax({
+            url: './ActionServlet',
+            type: 'POST',
+            data: {
+                todo: 'deconnecter'
+            },
+            dataType: 'json'
+        })
+                .done(function () {
+                    window.location = `index.html`;
+                }
+                )
+                .fail(function () { // Appel KO => erreur technique à gérer
+                })
+                .always(function () { // facultatif: appelé après le .done() ou le .fail()
+                });
+    });
 });
