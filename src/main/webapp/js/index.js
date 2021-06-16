726e7c66c53d796e314420f93754eb9e59040eae
LouisePietropaoli <louise.pietropaoli@gmail.com>
2021-06-16 10:00:40 +0200

    page accueil sans connexion

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -13,19 +13,21 @@ function afficherListeMediums() {
     })
     .done(function (response) {
         if (!response.erreur) {
-            let = "";
             console.log(response);
             const htmlMediumsListe = response.mediums.reduce(
                     (accumulateur, valeurCourante) => {
-                        console.log("valc courante=", valeurCourante);
+                                    console.log(valeurCourante);
                 return accumulateur +=
                         `<li class="carte-medium">
                         ${valeurCourante.genre} | ${valeurCourante.designation} | 
                         ${valeurCourante.type}<br/>
                         ${valeurCourante.presentation}
-                        <button class="icone-loupe" onClick="voirDetailsMediums(this)" data-id="${valeurCourante.id}">Voir détails</button>
+                        <button class="icone-loupe" onClick="voirDetailsMediums(this)" data-id="${valeurCourante.id}">
+                        <a href="#modal-details-medium" rel="modal:open">Voir détails</a>
+                        </button>
                         </li><br/>`;
             }, "");
+
             $("#liste-mediums").html(htmlMediumsListe);
         } else {
         }
@@ -49,6 +51,7 @@ function voirDetailsMediums(bouton) {
         .done(function (response) {
             if (!response.erreur) {
                   console.log(response);
+          $('#modal-details-medium-content').html(detailsMediumsHtml(response.medium));
             } else {
             }
         })
@@ -58,3 +61,18 @@ function voirDetailsMediums(bouton) {
         });              
     }
 
+function detailsMediumsHtml(medium) {
+    console.log("m", medium);
+    html = `Désignation: ${medium.designation} <br/> 
+            Genre: ${medium.genre} <br/>
+            Type: ${medium.type}<br/>`;
+    if (medium.type === "Astrologue") {
+        html += `Formation: ${medium.formation}<br/>
+                 Année de promotion: ${medium.anneePromotion}<br/>`;
+    } else if (medium.type === "Spirite") {
+        html += `Support: ${medium.support}<br/>`;
+    }
+    html += `Présentation: ${medium.presentation}`;
+    
+    return html;
+}
