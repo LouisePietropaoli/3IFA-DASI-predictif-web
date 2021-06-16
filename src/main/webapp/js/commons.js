cfd0040fe354c2ca40674b2ee3fc7dc0f6c4c244
LouisePietropaoli <louise.pietropaoli@gmail.com>
2021-06-16 10:23:34 +0200

    commons.js

--- src/main/webapp/js/commons.js
+++ src/main/webapp/js/commons.js
@@ -1,50 +1,15 @@
-$(document).ready(function () {
-    afficherListeMediums();
-    });
-    
-function afficherListeMediums() {
-    $.ajax({
-        url: './ActionServlet',
-        type: 'POST',
-        data: {
-            todo: 'lister-mediums'
-        },
-        dataType: 'json'
-    })
-    .done(function (response) {
-        if (!response.erreur) {
-            console.log(response);
-            const htmlMediumsListe = response.mediums.reduce(
-                    (accumulateur, valeurCourante) => {
-                                    console.log(valeurCourante);
-                return accumulateur +=
-                        `<li class="carte-medium">
-                        ${valeurCourante.genre} | ${valeurCourante.designation} | 
-                        ${valeurCourante.type}<br/>
-                        ${valeurCourante.presentation}
-                        <button class="icone-loupe" onClick="voirDetailsMediums(this)" data-id="${valeurCourante.id}">
-                        <a href="#modal-details-medium" rel="modal:open">Voir détails</a>
-                        </button>
-                        </li><br/>`;
-            }, "");
+/**
+ * 
+ * fonctions communes dans l'application
+ */
 
-            $("#liste-mediums").html(htmlMediumsListe);
-        } else {
-        }
-    })
-    .fail(function (error) {
-    })
-    .always(function () {
-    });
-}
-
-function voirDetailsMediums(bouton) {
+function voirDetailsMedium(idMedium) {
   $.ajax({
             url: './ActionServlet',
             type: 'POST',
             data: {
                 todo: 'recuperer-details-medium',
-                mediumId: bouton.dataset.id
+                mediumId: idMedium
             },
             dataType: 'json'
         })
