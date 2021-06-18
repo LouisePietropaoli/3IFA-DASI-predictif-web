db5696e708634b3f8038b914361bc6119f3e8679
LouisePietropaoli <louise.pietropaoli@gmail.com>
2021-06-18 12:03:32 +0200

    recup profil astral sans parametres

--- src/main/java/action/RecupererProfilAstral.java
+++ src/main/java/action/RecupererProfilAstral.java
@@ -3,29 +3,15 @@ package action;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import metier.data.Client;
-import metier.service.Service;
 
 public class RecupererProfilAstral extends Action {
 
     @Override
     public void executer(HttpServletRequest request) {
-        Long clientId;
-        try {
-            clientId = Long.valueOf(request.getParameter("clientId"));
-        } catch (Exception e) {
-            clientId = null;
-        }
-        Client client;
 
-        if (clientId == null) {
+        HttpSession session = request.getSession();
+        Client client = (Client) session.getAttribute("client");
 
-            HttpSession session = request.getSession();
-            client = (Client) session.getAttribute("client");
-        } else {
-            Service service = new Service();
-
-            client = service.rechercherClientParId(clientId);
-        }
         if (client == null) {
             request.setAttribute("client", null);
         } else {
